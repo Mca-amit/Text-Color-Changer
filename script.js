94cdb6a8749b94a9ce16044328d5930f27c3c0ee
AMIT KUMAR <amitkumaryadav6205169702@gmail.com>
2024-06-28 23:43:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let btn1=document.querySelector("#btn1");
+btn1.addEventListener("click", () => {
+    document.querySelector("p").style.color="red";
+} );
+
+let btn2=document.querySelector("#btn2");
+btn2.addEventListener("click", () => {
+    document.querySelector("p").style.color="green";
+} );
+
+let btn3=document.querySelector("#btn3");
+btn3.addEventListener("click", () => {
+    document.querySelector("p").style.color="orange";
+} );
